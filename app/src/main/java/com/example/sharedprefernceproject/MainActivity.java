3be6a85bd8ac56f3141a17280cca2c09d31f84cf
mohammad <m.zandieh1999@gmail.com>
2021-04-30 02:23:52 +0430

    add shared prefernce's method in UserManager class

--- app/src/main/java/com/example/sharedprefernceproject/MainActivity.java
+++ app/src/main/java/com/example/sharedprefernceproject/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.sharedprefernceproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/sharedprefernceproject/UserManager.java
@@ -0,0 +1,36 @@
+package com.example.sharedprefernceproject;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class UserManager {
+    private SharedPreferences sharedPreferences;
+
+    public UserManager(Context context) {
+        sharedPreferences = context.getSharedPreferences("user_information", Context.MODE_PRIVATE);
+    }
+
+    public void savedUserInformation (String fullName,
+                                      String email,
+                                      String gender){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("full_name", fullName);
+        editor.putString("email", email);
+        editor.putString("gender", gender);
+
+        editor.apply();
+    }
+
+
+    public String getFullName(){
+        return sharedPreferences.getString("full_name", "");
+    }
+
+    public String getEmail(){
+        return sharedPreferences.getString("email", "");
+    }
+
+    public String getGender(){
+        return sharedPreferences.getString("gender", "");
+    }
+}
